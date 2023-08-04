02886ad5ce4ee193f2cb5cdf8d0124762d80c5e5
KacsaTvT <kacsatvt@gmail.com>
2023-08-04 18:00:21 +0200

    Bug fix + 1 function

--- passwords.py
+++ passwords.py
@@ -1,11 +1,7 @@
-#password manager
 import mysql.connector
 
 passw = 3644
-jelszavak = [
-    ('steam', 'Zoknikukac123'),
-    ('tel', '3644')
-]
+
 database = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
@@ -15,36 +11,6 @@ database = mysql.connector.connect(
 cursor = database.cursor()
 
 #cursor.execute("CREATE TABLE pass (forras VARCHAR(255), jelszo VARCHAR(255))")
-'''
-cursor.execute("""
-    INSERT INTO passs VALUES
-        (uj1, uj25)
-""")
-'''
-
-"""
-uj1 = None
-uj2 = None
-useri = None
-
-folytatas = True
-while folytatas:
-    useri= int(input("Add meg a PIN-t: "))
-    if useri == '':
-        folytatas = False
-    if useri == passw:
-        #print(jelszavak)
-        uj1 = input("Mihez adsz meg új jelszót? ")
-        uj2 = input("Mi a jelszó?")
-        if uj1=='' or uj2=='':
-            folytatas = False
-    else:
-        folytatas = False
-"""
-
-#uj1 = input("Mihez adsz meg új jelszót? ")
-#uj2 = input("Mi a jelszó? ")
-
 
 userpass = None
 folytatas = True
@@ -55,21 +21,23 @@ command = None
 while folytatas:
     userpass=int(input("Mi a PIN? "))
     if userpass == passw:
-        command=input("Mit szeretnél tenni? (megtekintés / hozzáadás) ")
-        if command== '':
-            folytatás=False
+        command=input("Mit szeretnél tenni? (megtekintés / hozzáadás / kilépés) ")
         if command=="hozzáadás":
             uj1 = input("Mihez adsz meg új jelszót? ")
             uj2 = input("Mi a jelszó? ")
-            cursor.execute("INSERT INTO pass (forras, jelszo) VALUES (%s, %s)", (uj1,uj2))
-            database.commit()
-        if uj1 or uj2 == '':
-            folytatas=False
-        else:
-            print("Feltöltve")
+            if uj1 =='' or uj2=='':
+                folytatas=False
+            else:
+                cursor.execute("INSERT INTO pass (forras, jelszo) VALUES (%s, %s)", (uj1,uj2))
+                database.commit()
         if command=="megtekintés":
-            cursor.execute("..........!!!!!!")
+            cursor.execute("SELECT * FROM pass")
+            result= cursor.fetchall()
+            for row in result:
+                print(row, '\n')
     else:
+        folytatas=False
+    if command== '' or command=='kilépés':
         folytatas=False
 
 print("A program vége")
